b32b851ac36bbd6d9a2b00074e95b5a2b15edc57
Mauro Salguero <mauro.salguero@udla.edu.ec>
2023-10-25 07:39:59 -0500

    Tabla y atributos creados

--- Models/MauroSalguero_tabla.cs
+++ Models/MauroSalguero_tabla.cs
@@ -1,6 +1,32 @@
-﻿namespace MauroSalguero_1P.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MauroSalguero_1P.Models
 {
     public class MauroSalguero_tabla
     {
+        [Required(ErrorMessage = "Debe contener un nombre")]
+        [Display(Name = "Nombre")]
+        [RegularExpression(@"^[a-zA-Z0-9]*$", ErrorMessage = "Solo se aceptan caracteres alfanumericos")]
+        public string? msName { get; set; }
+        [Display(Name = "Cedula")]
+        [MaxLength(10, ErrorMessage = "No debe superar los 10 digitos")]
+        [RegularExpression("^[0-9]{10}$", ErrorMessage = "Solo se aceptan caracteres numericos")]
+        public string msID { get; set; }
+        [Display(Name = "Es estudiante Udla?")]
+        [Required]
+        public bool msUdlaStudent { get; set; }
+        [Display(Name = "Edad")]
+        [Range(10, 100, ErrorMessage = "Edad Invalida")]
+        public decimal msEdad { get; set; }
+        [Display(Name = "Fecha de Nacimiento")]
+        [DataType(DataType.DateTime)]
+        public DateTime msDateOfBirth { get; set; }
+        [Display(Name = "Promedio")]
+        [Range(0, 10, ErrorMessage = "Debe estar entre 0(cero) y 10(diez)")]
+        public decimal msGrade { get; set; }
+
+
+
+
     }
 }
